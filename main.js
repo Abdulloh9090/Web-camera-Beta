cb415d53f2b9d5efa53c731c855e9216a6eefc8e
Abdulloh9090 <152011956+Abdulloh9090@users.noreply.github.com>
2023-12-02 17:23:17 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const video = document.getElementById('videoElement');
+
+    if (navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices.getUserMedia({ video: true })
+            .then((stream) => {
+                video.srcObject = stream;
+            })
+            .catch((error) => {
+                console.error('Error accessing camera:', error);
+            });
+    } else {
+        console.error('getUserMedia is not supported on your browser');
+    }
+});
